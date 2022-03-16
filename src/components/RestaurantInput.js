956f59180a88331b24ad7a290bbde592db262d50
Samantha Diaz <samidiaz3@gmail.com>
2022-03-16 10:01:05 -0400

    commit tree 852e6ec4c7e798fe9e47a8c90c2418072819e87b

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -50,5 +51,13 @@ export class RestaurantInput extends Component {
 };
 
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  }
+}
+
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput)
